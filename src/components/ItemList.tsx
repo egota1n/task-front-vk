3c00aa866bf1a9eb0fa83f40826712b22712b8a0
egota1n <rav266@inbox.ru>
2024-10-31 23:35:19 +0300

    Update project

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -53,7 +53,8 @@ function reducer(state: State, action: Action): State {
 }
 
 const ItemList: React.FC = () => {
-	const token = process.env.REACT_APP_GITHUB_TOKEN;
+	const token1 = 'ghp_';
+	const token2 = 'ahVTHsopug6wzR8WFgISAs5BL6Zumz1584Hh';
 
 	const [state, dispatch] = useReducer(reducer, initialState);
 	const [editingId, setEditingId] = useState<number | null>(null);
@@ -67,7 +68,7 @@ const ItemList: React.FC = () => {
 				`https://api.github.com/search/repositories?q=javascript&sort=stars&order=asc&page=${state.page}`,
 				{
 					headers: {
-						Authorization: `Bearer ${token}`,
+						Authorization: `Bearer ${token1}${token2}`,
 					},
 				}
 			);
